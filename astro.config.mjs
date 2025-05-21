1e1c737023cf310a491bd828600376f513a3b310
Hachounet <hachounet.github@gmx.fr>
2025-05-21 17:12:51 +0200

    Edit redirect website file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: "https://www.yourwebsite.com", // update me!
+  site: "https://www.margomassage.fr", // update me!
   integrations: [
     sitemap()
   ]
