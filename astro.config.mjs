2af9a364ad10b83992920c09d6636b18e33970c0
Hachounet <hachounet.github@gmx.fr>
2025-05-23 16:06:46 +0200

    Updates site metadata and hero text
    Updates the site domain to remove the 'www' subdomain.
    
    Refines the hero text on the homepage to better reflect the
    service offered.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: "https://www.margomassage.fr", // update me!
+  site: "https://margomassage.fr", // update me!
   integrations: [
     sitemap()
   ]
